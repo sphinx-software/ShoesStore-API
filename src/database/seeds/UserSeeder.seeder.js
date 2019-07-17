a0363574191ebb74dc908e1aea3145660dee054c
linhtv97 <linh.cntt097@gmail.com>
2019-07-17 15:12:58 +0700

    Changle seeder user

--- src/database/seeds/UserSeeder.seeder.js
+++ src/database/seeds/UserSeeder.seeder.js
@@ -1,6 +1,7 @@
 import Profile from "../../ShoesStore/Profile/Profile";
 import faker from "faker";
 import {inject} from "@fusion.io/framework";
+import Credential from "../../Http/Auth/Credential";
 
 
 export default class UserSeeder {
@@ -10,16 +11,14 @@ export default class UserSeeder {
             await Profile.query().insert({
                 name: faker.name.findName(),
                 phone: faker.phone.phoneNumber(),
-                email: faker.internet.email(),
+                dob:faker.date.past(),
+                credential_id:faker.random.number(),
                 gender: faker.random.boolean().toString(),
                 address: [
                     faker.address.streetAddress(),
                     faker.address.streetAddress()
                 ],
                 avatar: faker.image.avatar(),
-                created_at:new Date(),
-                updated_at:new Date(),
-                delete_at:faker.random.boolean().toString()
             });
         }
     }
